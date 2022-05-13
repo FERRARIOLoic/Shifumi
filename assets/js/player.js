88472f33684c0e387f2cfc94fcda361585bdc55d
FERRARIOloic <ferrarioloic@gmail.com>
2022-05-13 11:21:19 +0200

    commit

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,16 +1,18 @@
+// Function of music player
 let play = () => {
-    const audio = document.querySelector('#audio');
-    if(soundAmbiance.src== 'http://127.0.0.1:5000/assets/img/speakerOff.png' || soundambiance.src=='assets/img/speakerOff.png'){
-        soundAmbiance.src = 'assets/img/volume.png';
-        audio.volume = 1;
+    const audio = document.querySelector("#audio");
+    if(soundAmbiance.src == 'http://127.0.0.1:5500/assets/img/speakerOff.png' || soundAmbiance.src == 'assets/img/speakerOff.png'){
+        soundAmbiance.src = 'assets/img/speakerOn.png';
+        audio.volume = 1; // 0.5 = 50% 1 = 100%
         audio.play();
-    }else{
+    }
+    else{
         soundAmbiance.src = 'assets/img/speakerOff.png';
         audio.pause();
         audio.currentTime = 0;
-    
     }
 }
 
+// Set audio or not when the user click on the sound button
 let soundAmbiance = document.getElementById('soundAmbiance');
 soundAmbiance.addEventListener('click', play);
--- assets/js/script.js
+++ assets/js/script.js
@@ -131,3 +131,4 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
     myInput.focus()
 })
+
